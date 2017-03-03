b42a5c897b00d1ac1064799fda819fd735aab8e6
El Amine <el.aittali@gmail.com>
2017-03-03 19:21:26 +0100

    final

--- src/main/java/fr/robot/twitterClient/config/GlobalSettings.java
+++ src/main/java/fr/robot/twitterClient/config/GlobalSettings.java
@@ -4,9 +4,9 @@ public class GlobalSettings {
 
 	// == #database connection
 	public static final  String DRIVER_CLASS_NAME="org.postgresql.Driver";
-	public static final  String URL="jdbc:postgresql://localhost:5432/robot";
-	public static final  String	USERNAME = "postgres";
-	public static final  String	PASSWORD = "postgres";
-	
+	public static final  String URL="jdbc:postgres://ec2-54-235-181-120.compute-1.amazonaws.com:5432/dc8bd7p629f7j4";
+	public static final  String	USERNAME = "yhiumcckxaezco";
+	public static final  String	PASSWORD = "a970a0c72b9261170f1d8393b728cd572648a3877a03781bd9b683a5503cd706";
+
 	public static final String ENTITY_PACKAGES_TO_SCAN = "fr.robot.twitterClient.dataaccess.entity";
 }
